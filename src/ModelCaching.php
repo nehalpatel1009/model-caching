590248c0cf549e34a13480d38ab9071fd93f09b5
Nehal <nehal@krishaweb.com>
2025-02-15 14:19:45 +0530

    Add Github Repo

--- src/ModelCaching.php
+++ src/ModelCaching.php
@@ -2,4 +2,19 @@
 
 namespace Nehal\ModelCaching;
 
-class ModelCaching {}
+use Illuminate\Database\Eloquent\Builder;
+
+class ModelCaching
+{
+    protected static $builder = Builder::class;
+
+    public static function useEloquentBuilder(string $builder) : void
+    {
+        self::$builder = $builder;
+    }
+
+    public static function builder()
+    {
+        return self::$builder;
+    }
+}
